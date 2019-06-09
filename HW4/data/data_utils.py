d874ecf7f11c90c37bdc3b7d2c9fa66779ce629d
I <danibachar89@gmail.com>
2019-06-09 17:02:07 +0300

    HW4 upload

---
+++ HW4/data/data_utils.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+def load_imagenet_val(num=None):
+    """Load a handful of validation images from ImageNet.
+    Inputs:
+    - num: Number of images to load (max of 25)
+    Returns:
+    - X: numpy array with shape [num, 224, 224, 3]
+    - y: numpy array of integer image labels, shape [num]
+    - class_names: dict mapping integer label to class name
+    """
+    imagenet_fn = 'data/imagenet_val_25.npz'
+    f = np.load(imagenet_fn)
+    X = f['X']
+    y = f['y']
+    class_names = f['label_map'].item()
+    if num is not None:
+        X = X[:num]
+        y = y[:num]
+    return X, y, class_names
